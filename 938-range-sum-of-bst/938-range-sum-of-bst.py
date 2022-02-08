7673195474cd97291bd6c889880283b4e6bf4d1a
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-02-08 02:22:38 -0500

    Time: 217 ms (72.28%), Space: 23.1 MB (7.30%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.py
+++ 938-range-sum-of-bst/938-range-sum-of-bst.py
@@ -1,31 +1,14 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-        queue = [root]
-        res = 0
-        
-        while queue:
-            node = queue.pop(0)
-            print(node.val)
-            if node.val>=low and node.val<=high:
-                res+=node.val
-                if node.val == high and node.left is not None:
-                    queue.append(node.left)
-                elif node.val == low and node.right is not None:
-                    queue.append(node.right)
-                else:
-                    if node.left is not None:
-                        queue.append(node.left)
-                    if node.right is not None:
-                        queue.append(node.right)
-            elif node.val<low and node.right is not None:
-                queue.append(node.right)
-            elif node.val>high and node.left is not None:
-                queue.append(node.left)
-        return res
-        
+class Solution(object):
+    def rangeSumBST(self, root, L, R):
+        def dfs(node):
+            if node:
+                if L<= node.val <= R:
+                    self.ans += node.val
+                    #print(node.val)
+                if node.val>L:
+                    dfs(node.left)
+                if node.val<R:
+                    dfs(node.right)
+        self.ans = 0
+        dfs(root)
+        return self.ans
