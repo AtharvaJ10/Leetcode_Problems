ab2ec3aeec54733e5157c39c1ad2ce9e80c74edf
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-02-10 09:03:18 -0500

    Time: 1041 ms (37.22%), Space: 20.4 MB (31.92%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -1,21 +1,12 @@
-from collections import defaultdict
-import sys,math
+import math
 class Solution:
-    def distance(self,x,y):
-        return math.sqrt(y**2 + x**2)
-    
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        d = defaultdict(list)
-        
+        h = []
         for i in range(len(points)):
-            temp = self.distance(points[i][1],points[i][0])
-            d[temp].append([points[i][0],points[i][1]])
-            
-        res = []
-        for key in sorted(d.keys()):
-            for value in d[key]:
-                if len(res)==k:
-                    return res
-                else:
-                    res.append(value)
-        return res
+            dist = math.sqrt(points[i][0]**2+points[i][1]**2)
+            temp = [points[i][0],points[i][1]]
+            if len(h)<k:
+                heapq.heappush(h,(-dist,temp))
+            else:
+                heapq.heappushpop(h,(-dist,temp))
+        return [h[i][1] for i in range(len(h))]
