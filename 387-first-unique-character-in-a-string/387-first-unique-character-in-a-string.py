c5cdcbbbe59580fc31a13598e7a750e416b845a7
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-03-23 00:46:51 -0400

    Time: 192 ms (35.53%), Space: 14.2 MB (27.80%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.py
@@ -3,11 +3,16 @@ class Solution:
         d = dict()
         for i in range(len(s)):
             if s[i] in d:
-                d[s[i]]+=1
+                d[s[i]][0]+=1
             else:
-                d[s[i]] = 1
-        
-        for i in range(len(s)):
-            if d[s[i]]==1:
-                return i
-        return -1
+                d[s[i]] = [1,i]
+                
+        min1 = len(s)
+        for i in d:
+            print(d)
+            if d[i][0]==1 and d[i][1]<min1:
+                min1=d[i][1]
+        if min1==len(s):
+            return -1
+        return min1
+                
