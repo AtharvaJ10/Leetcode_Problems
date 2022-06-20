15fd69d1656f9ab4dba85bbac63f302c51dfbb61
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-06-20 00:29:17 -0400

    Time: 218 ms (80.52%), Space: 15 MB (51.24%) - LeetHub

--- 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.py
+++ 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.py
@@ -1,22 +1,12 @@
 class Solution:
     def minFlipsMonoIncr(self, s: str) -> int:
-          #     Explanation: Our array "S" must be generally sorted as [0]*a + [1]*b
-        #         - Examples: 000, 001, 011, 111
-        #         - We can sweep through each configuration computing their cost
-        # To get started, we assume that we have an array of 1's (111), and every zero must be changed
-        # Compute this cost:
-        cost = s.count("0")
-        # This is our baseline guess for the best answer
+        cost = s.count('0')
         best = cost
-        #     Now we will start allowing zeros up to x=S[i] (last allowed zero)
-        #         Check how many zeros can stay for free, and..
-        #         How many 1's must be force to change as we advance
-        #
-        for x in s: # last 0 allowed
-            if x=='0':
-                cost -= 1 # this "zero" no longer is a problem (subtract from original cost)
+        for i in s:
+            if i=='0':
+                cost-=1
             else:
-                cost += 1 # this "one" must be forced to change (higher cost)
+                cost+=1
             best = min(best,cost)
         return best
         
