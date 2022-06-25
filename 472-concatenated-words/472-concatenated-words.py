95bcc062ff8766c0dcf7ad11a552433dea2c80f5
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-06-25 16:11:56 -0400

    Time: 1370 ms (15.32%), Space: 30 MB (29.46%) - LeetHub

--- 472-concatenated-words/472-concatenated-words.py
+++ 472-concatenated-words/472-concatenated-words.py
@@ -1,16 +1,50 @@
 class Solution:
-    def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
-        words = set(words)
-        memo = {}
-        
-        def dfs(word):
-            if word in memo:
-                return memo[word]
-            memo[word] = False
-            for i in range(1, len(word)):
-                if word[:i] in words and (word[i:] in words or dfs(word[i:])):
-                    memo[word] = True
-                    break
-            return memo[word]
+    def findAllConcatenatedWordsInADict(self, words):
+        t, memo, res = Trie(), {}, []
+        for word in words:
+            t.add(word)
+        for word in words:
+            if t.helper(word, 0, len(word) - 1, 0, memo):
+                res.append(word)
+        return res
 
-        return [word for word in words if dfs(word)]
+
+class Node:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+
+
+class Trie:
+    def __init__(self):
+        self.root = Node()
+
+    def add(self, word):
+        p = self.root
+        for c in word:
+            if c not in p.children:
+                p.children[c] = Node()
+            p = p.children[c]
+        p.is_end = True
+
+
+    def helper(self, word, st, end, cnt, memo):
+        p = self.root
+        curr = word[st: end + 1]
+        if curr in memo: 
+            return memo[curr]
+        
+        for x in range(st, end + 1):
+            if word[x] in p.children:
+                p = p.children[word[x]]
+                if p.is_end:
+                    if x == end:
+                        return cnt >= 1
+                    if self.helper(word, x + 1, end, cnt + 1, memo):
+                        memo[curr] = True
+                        return True
+            else:
+                break
+                
+        memo[curr] = False
+        return False
