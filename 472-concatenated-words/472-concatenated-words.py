cd9e89ba845ba914bcdbfe630e0760e074074c2e
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-06-25 15:09:49 -0400

    Time: 629 ms (54.61%), Space: 32.3 MB (25.22%) - LeetHub

--- 472-concatenated-words/472-concatenated-words.py
+++ 472-concatenated-words/472-concatenated-words.py
@@ -1,20 +1,16 @@
 class Solution:
-    def findAllConcatenatedWordsInADict(self, words: 'List[str]') -> 'List[str]':
-        d = set(words)
+    def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
+        words = set(words)
         memo = {}
+        
         def dfs(word):
             if word in memo:
                 return memo[word]
             memo[word] = False
             for i in range(1, len(word)):
-                prefix = word[:i]
-                suffix = word[i:]
-                if prefix in d and suffix in d:
-                    memo[word] = True 
-                    break
-                if prefix in d and dfs(suffix):
+                if word[:i] in words and (word[i:] in words or dfs(word[i:])):
                     memo[word] = True
-                    memo[suffix] = True
                     break
-            return memo[word] 
-        return [word for word in words if dfs(word)] 
+            return memo[word]
+
+        return [word for word in words if dfs(word)]
