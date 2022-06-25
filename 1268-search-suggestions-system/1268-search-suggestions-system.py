7c21b7cd627f062b24c41e4b9de067778d8928ff
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-06-25 00:40:32 -0400

    Time: 252 ms (48.19%), Space: 17.1 MB (82.21%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,14 +1,24 @@
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        products = sorted(products)
+        products.sort()
+        str1 = ''
         res = []
-        for i in range(len(searchWord)):
+        for i in searchWord:
+            str1+=i
+            index = self.binary_search(products,str1)
             temp = []
-            for j in range(len(products)):
-                if searchWord[:i+1]==products[j][:i+1]:
+            for j in range(min(index+3,len(products))):
+                if products[j].startswith(str1):
                     temp.append(products[j])
-            res.append(temp.copy()[:3])
+            res.append(temp)
         return res
-            
-            
-        
+    
+    def binary_search(self,products,target):
+        low,high = 0,len(products)-1
+        while low<high:
+            mid = (low+high)//2
+            if products[mid]<target:
+                low = mid+1
+            else:
+                high = mid
+        return low
