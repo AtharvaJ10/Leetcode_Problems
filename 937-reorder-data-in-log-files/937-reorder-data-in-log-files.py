10a3ed00fb8e5bf5aa3232e36657191bd391b4ac
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-06-26 11:28:40 -0400

    Time: 50 ms (61.11%), Space: 14.1 MB (7.47%) - LeetHub

--- 937-reorder-data-in-log-files/937-reorder-data-in-log-files.py
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.py
@@ -1,18 +1,10 @@
 class Solution:
     def reorderLogFiles(self, logs: List[str]) -> List[str]:
-        """letters,digits= [],[]
-        for i in logs:
-            if i[-1].isdigit():
-                digits.append(i)
-            else:
-                letters.append(i)
-        letters = sorted(letters, key = lambda row: (row.split()[1:], row.split()[0]))
-        return letters+digits"""
-        
-        def custom_sort(log):
-            left, right = log.split(" ",1)
-            if right[0].isdigit():
+        def solve(log):
+            if log[-1].isdigit():
                 return (1,)
             else:
+                left, right = log.split(" ",1)
                 return (0, right, left)
-        return sorted(logs, key = lambda l: custom_sort(l))
+        
+        return sorted(logs, key = solve)
