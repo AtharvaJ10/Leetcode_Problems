9b81bc53eecdcac5fe2133723b96a77806efa388
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-06-26 14:23:53 -0400

    Time: 441 ms (54.40%), Space: 16.8 MB (38.69%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,38 +1,21 @@
-import collections
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        """def dfs(i,j):
-            if i>=rows or i<0 or j>=cols or j<0 or visited[i][j] or grid[i][j]=="0":
+        def dfs(i,j):
+            if i<0 or i>=rows or j<0 or j>=cols or grid[i][j]=="0" or visited[i][j]:
                 return
+            
             visited[i][j] = True
             dfs(i,j+1)
-            dfs(i-1,j)
-            dfs(i,j-1)
             dfs(i+1,j)
-            
-        rows, cols = len(grid), len(grid[0])
-        visited = [[False for i in range(len(grid[0]))] for j in range(len(grid))]
+            dfs(i,j-1)
+            dfs(i-1,j)
+        
         count = 0
+        rows, cols = len(grid), len(grid[0])
+        visited = [[False for _ in range(cols)] for _ in range(rows)]
         for i in range(rows):
             for j in range(cols):
                 if not visited[i][j] and grid[i][j]=="1":
                     dfs(i,j)
                     count+=1
-        return count"""
-        
-        count = 0
-        queue = collections.deque([])
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j]=="1":
-                    grid[i][j]=0
-                    queue.append((i,j))
-                    count+=1
-                    while queue:
-                        I,J = queue.popleft()
-                        for k,l in [I+1,J], [I,J-1], [I-1,J], [I,J+1]:
-                            if 0<=k<len(grid) and 0<=l<len(grid[0]) and grid[k][l]=="1":
-                                queue.append((k,l))
-                                grid[k][l]=0
         return count
-        
