196819266ea7f427eb907c3d8ddc08899b4edb84
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-07-07 18:55:33 -0400

    Time: 399 ms (70.26%), Space: 16.2 MB (97.42%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,6 +1,7 @@
+from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def dfs(i,j):
+        """def dfs(i,j):
             if i<0 or i>=rows or j<0 or j>=cols or visited[i][j] or grid[i][j]=="0":
                 return
             visited[i][j] = True
@@ -17,6 +18,24 @@ class Solution:
                 if not visited[i][j] and grid[i][j]=="1":
                     dfs(i,j)
                     count+=1
+        return count"""
+        
+        rows, cols, count = len(grid), len(grid[0]), 0
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == "1":
+                    queue = deque([])
+                    queue.append([i,j])
+                    grid[i][j] = 0
+                    count+=1
+                    while queue:
+                        k,l = queue.popleft()
+                        for x,y in [k+1,l], [k,l-1], [k-1,l], [k,l+1]:
+                            if 0<=x<rows and 0<=y<cols and grid[x][y]=="1":
+                                queue.append([x,y])
+                                grid[x][y] = 0
         return count
+                                
+        
                     
             
