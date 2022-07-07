a1e2c6d8545a0382d06ad0d070fa70f92cff8a80
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-07-07 17:37:30 -0400

    Time: 2619 ms (74.22%), Space: 14.5 MB (11.56%) - LeetHub

--- 2272-substring-with-largest-variance/2272-substring-with-largest-variance.py
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.py
@@ -1,36 +1,35 @@
 class Solution:
     def largestVariance(self, s: str) -> int:
         def kadane(array):
-            res, current = 0,0
             seen = False
+            res, sum1 = 0, 0
             for i in array:
-                if i==-1:
+                if i== -1:
                     seen = True
-                current+=i
+                sum1+=i
                 if seen:
-                    res = max(res, current)
+                    res = max(res, sum1)
                 else:
-                    res = max(res, current-1)
-                if current<0:
-                    current = 0
+                    res = max(res, sum1-1)
+                if sum1<0:
+                    sum1 = 0
                     seen = False
             return res
         
-        set1 = set(s)
-        str1 = ''.join(set1)
-        
-        variance = 0
+        str1 = set(s)
+        str1 = ''.join(str1)
+        res = 0
         for i in range(len(str1)-1):
-            for j in range(i+1, len(str1)):
-                x = str1[i]
-                y = str1[j]
-                temp = []
+            for j in range(i+1,len(str1)):
+                x,y = str1[i], str1[j]
+                array = []
                 for k in s:
                     if k!=x and k!=y:
                         continue
                     elif k==x:
-                        temp.append(1)
+                        array.append(1)
                     else:
-                        temp.append(-1)
-                variance = max(variance, kadane(temp), kadane([-x for x in temp]))
-        return variance
+                        array.append(-1)
+                print(array)
+                res = max(res, kadane(array), kadane([-x for x in array]))
+        return res
