6d7e36fe310cc2abb3a2e330fa86aaed99d8fcaa
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-07-07 16:00:04 -0400

    Time: 185 ms (59.50%), Space: 14.3 MB (12.81%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.py
@@ -3,18 +3,19 @@ class Solution:
         stack = []
         res = 0
         for i in range(len(nums)+1):
-            while stack and (i ==len(nums) or nums[stack[-1]]>nums[i]):
+            while stack and (i==len(nums) or nums[stack[-1]]<nums[i]):
+                j = stack.pop()
+                k = stack[-1] if stack else -1
+                res+= nums[j] * (i-j) * (j-k)
+            stack.append(i)
+            
+        stack = []
+        for i in range(len(nums)+1):
+            while stack and (i==len(nums) or nums[stack[-1]]>nums[i]):
                 j = stack.pop()
                 k = stack[-1] if stack else -1
                 res-= nums[j] * (i-j) * (j-k)
             stack.append(i)
         
-        stack = []
-        for i in range(len(nums)+1):
-            while stack and (i ==len(nums) or nums[stack[-1]]<nums[i]):
-                j = stack.pop()
-                k = stack[-1] if stack else -1
-                res+= nums[j] * (i-j) * (j-k)
-            stack.append(i)
         return res
-        
+            
