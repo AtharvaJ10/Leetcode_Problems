47506d7a7c7d713fbe917edf0fab44f5a2a086bb
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-07-07 19:09:48 -0400

    Time: 80 ms (96.12%), Space: 17.1 MB (55.85%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,41 +1,26 @@
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.children = {}
-        self.suggestions = []
-        
-class Trie:
-    def __init__(self):
-        self.root = Node(None)
-        
-    def insert(self, product):
-        root = self.root
-        for i in product:
-            if i not in root.children:
-                root.children[i] = Node(i)
-            root = root.children[i]
-            if len(root.suggestions)<3:
-                root.suggestions.append(product)
-    
-    def find(self, word):
-        root = self.root
-        res = []
-        for i in word:
-            if i in root.children:
-                root = root.children[i]
-                res.append(root.suggestions)
-            else:
-                break
-        rem = len(word) - len(res)
-        for i in range(rem):
-            res.append([])
-        return res
-            
-
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        def binary_search(low, target):
+            hi = len(products)-1
+            while low<hi:
+                mid = (low+hi)//2
+                if products[mid]<target:
+                    low = mid+1
+                else:
+                    hi = mid
+            return low
+        
         products.sort()
-        t = Trie()
-        for i in products:
-            t.insert(i)
-        return t.find(searchWord)
+        temp = ""
+        res = []
+        low = 0
+        for i in searchWord:
+            temp+=i
+            index = binary_search(low, temp)
+            low = index
+            array = []
+            for j in range(index, min(index+3, len(products))):
+                if products[j].startswith(temp):
+                    array.append(products[j])
+            res.append(array)
+        return res
