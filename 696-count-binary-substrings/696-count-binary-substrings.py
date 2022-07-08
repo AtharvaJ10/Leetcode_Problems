3c4eac86149585249b52cbb001a6003b47c2aeb3
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-07-07 20:40:32 -0400

    Time: 373 ms (21.32%), Space: 14.5 MB (83.44%) - LeetHub

--- 696-count-binary-substrings/696-count-binary-substrings.py
+++ 696-count-binary-substrings/696-count-binary-substrings.py
@@ -3,11 +3,10 @@ class Solution:
         res, prev, curr = 0, 0, 1
         for i in range(1, len(s)):
             if s[i]!=s[i-1]:
-                res += min(prev, curr)
+                res+= min(prev, curr)
                 prev = curr
                 curr = 1
             else:
                 curr+=1
-        res += min(prev, curr)
+        res+= min(prev, curr)
         return res
-                
