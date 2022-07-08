c37668b60ed1e02412d271c3829cb42cd6493e03
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-07-07 20:29:59 -0400

    Time: 189 ms (75.98%), Space: 14.4 MB (84.36%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
@@ -3,19 +3,17 @@ class Solution:
     def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
         d = defaultdict(int)
         max1 = 0
-        for i in boxTypes:
-            d[i[1]]+= i[0]
-            max1 = max(max1, i[1])
+        for i in range(len(boxTypes)):
+            d[boxTypes[i][1]] += boxTypes[i][0]
+            max1 = max(max1, boxTypes[i][1])
         
-        units = 0
+        res = 0
         while max1>0:
-            if d[max1]>0:
+            if max1 in d:
                 fit = min(truckSize, d[max1])
-                units+= fit*max1
+                res+= fit*max1
                 truckSize-=fit
                 if truckSize==0:
-                    return units
+                    return res
             max1-=1
-        return units
-                
-        
+        return res
