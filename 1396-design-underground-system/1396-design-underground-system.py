a587a67cf0b04aa3b258bf19fd6dc919e229f338
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-04 18:08:35 -0400

    Time: 356 ms (55.16%), Space: 23.8 MB (87.81%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.py
+++ 1396-design-underground-system/1396-design-underground-system.py
@@ -2,22 +2,21 @@ from collections import defaultdict
 class UndergroundSystem:
 
     def __init__(self):
-        self.dito={}
-        self.place=defaultdict(lambda:[0,0])
-        
+        self.check = {}
+        self.result=defaultdict(lambda:[0,0])
 
     def checkIn(self, id: int, stationName: str, t: int) -> None:
-        self.dito[id]=[stationName,t]
+        self.check[id] = [stationName,t]
 
     def checkOut(self, id: int, stationName: str, t: int) -> None:
-        retrieve=self.dito[id]
-        self.place[(retrieve[0],stationName)][0]+=t-retrieve[1]
-        self.place[(retrieve[0],stationName)][1]+=1
-        
+        checked_in = self.check.pop(id)
+        route = (checked_in[0], stationName)
+        self.result[route][0]+=t-checked_in[1]
+        self.result[route][1]+=1
 
     def getAverageTime(self, startStation: str, endStation: str) -> float:
-        hold=self.place[(startStation,endStation)]
-        return float(hold[0]/hold[1])
+        route = (startStation, endStation)
+        return self.result[route][0]/self.result[route][1]
 
 
 # Your UndergroundSystem object will be instantiated and called as such:
