e67b5eae7b7498e03db5940ccbb564587c9a5926
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-05 16:39:08 -0400

    Time: 344 ms (47.92%), Space: 14.7 MB (92.73%) - LeetHub

--- 1656-design-an-ordered-stream/1656-design-an-ordered-stream.py
+++ 1656-design-an-ordered-stream/1656-design-an-ordered-stream.py
@@ -1,15 +1,15 @@
 class OrderedStream:
 
     def __init__(self, n: int):
-        self.result = [None]*(n+1)
+        self.res = [None] * (n+1)
         self.start = 1
-
+        
     def insert(self, idKey: int, value: str) -> List[str]:
-        self.result[idKey] = value
+        self.res[idKey] = value
         res = []
         if idKey==self.start:
-            while self.start<len(self.result) and self.result[self.start] is not None:
-                res.append(self.result[self.start])
+            while self.start<len(self.res) and self.res[self.start]:
+                res.append(self.res[self.start])
                 self.start+=1
         return res
 
