206ce39d9e5cb71062472d44081c3a368172c37d
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-05 23:43:09 -0400

    Time: 48 ms (47.18%), Space: 13.9 MB (71.86%) - LeetHub

--- 394-decode-string/394-decode-string.py
+++ 394-decode-string/394-decode-string.py
@@ -1,24 +1,18 @@
 class Solution:
     def decodeString(self, s: str) -> str:
         stack = []
-        current_string = ""
         k = 0
-
-        for char in s:
-            if char == "[":
-                # Just finished parsing this k, save current string and k for when we pop
-                stack.append((current_string, k))
-                # Reset current_string and k for this new frame
-                current_string = ""
+        current = ""
+        for i in s:
+            if i=="[":
+                stack.append((current,k))
+                current = ""
                 k = 0
-            elif char == "]":
-                # We have completed this frame, get the last current_string and k from when the frame 
-                # opened, which is the k we need to duplicate the current current_string by
-                last_string, last_k = stack.pop(-1)
-                current_string = last_string + last_k * current_string
-            elif char.isdigit():
-                k = k * 10 + int(char)
+            elif i.isdigit():
+                k = k*10 + int(i)
+            elif i =="]":
+                last_s, last_k = stack.pop()
+                current = last_s + current*last_k
             else:
-                current_string += char
-    
-        return current_string        
+                current+=i
+        return current
