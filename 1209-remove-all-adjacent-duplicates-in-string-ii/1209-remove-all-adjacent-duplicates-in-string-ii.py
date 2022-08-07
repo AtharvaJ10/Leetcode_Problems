ce7f2c5f1bb4894d7994101e64cc9e07ff01e189
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-07 11:04:22 -0400

    Time: 251 ms (20.86%), Space: 19.6 MB (12.34%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.py
@@ -10,25 +10,16 @@ class Solution:
                 stack.pop()
         return ''.join(c*k for c,k in stack)"""
         
-        n = len(s)
-        if n == 0:
-            return ""
-        
-        count = [0] * n
-        
-        i = 0
+        i=0
+        count = [0] * len(s)
         s = list(s)
-        
-        for j in range(n):
+        for j in range(len(s)):
             s[i] = s[j]
-            if i >0 and s[i-1] == s[j]:
-                count[i] = count[i-1] + 1
+            if i>0 and s[i-1] == s[j]:
+                count[i] = count[i-1]+1
             else:
                 count[i] = 1
-                
-            if count[i] == k:
-                i -= k
-                
-            i += 1
-            
-        return "".join(s[:i])
+            if count[i]==k:
+                i-=k
+            i+=1
+        return ''.join(s[:i])
