de3862920c67ac8b79624e07532810048cf12fd5
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-07 12:53:50 -0400

    Time: 692 ms (35.44%), Space: 30.3 MB (32.49%) - LeetHub

--- 1583-count-unhappy-friends/1583-count-unhappy-friends.py
+++ 1583-count-unhappy-friends/1583-count-unhappy-friends.py
@@ -2,8 +2,8 @@ class Solution:
     def unhappyFriends(self, n: int, preferences: List[List[int]], pairs: List[List[int]]) -> int:
         d, p = {}, preferences
         for x, y in pairs:
-            d[x] = p[x][:p[x].index(y)]
-            d[y] = p[y][:p[y].index(x)]
+            d[x] = set(p[x][:p[x].index(y)])
+            d[y] = set(p[y][:p[y].index(x)])
         
         res = 0
         for x in d:
