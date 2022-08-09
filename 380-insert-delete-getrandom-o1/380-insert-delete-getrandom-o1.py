9b530f8a301033707c839e0bd3056c08cdac1904
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-09 14:48:58 -0400

    Time: 775 ms (37.11%), Space: 61 MB (95.41%) - LeetHub

--- 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.py
@@ -2,29 +2,29 @@ import random
 class RandomizedSet:
 
     def __init__(self):
-        self.d = {}
-        self.l = []
+        self.values = []
+        self.indexes = defaultdict(int)
 
     def insert(self, val: int) -> bool:
-        if val in self.d:
-            return False
-        self.l.append(val)
-        self.d[val] = len(self.l)-1
-        return True
+        if val not in self.indexes:
+            self.values.append(val)
+            self.indexes[val] = len(self.values)-1
+            return True
+        return False
 
     def remove(self, val: int) -> bool:
-        if val not in self.d:
+        if val not in self.indexes:
             return False
-        last = self.l[-1]
-        index = self.d[val]
-        self.l[index] = last
-        self.d[last] = index
-        self.l.pop()
-        del self.d[val]
+        out_index = self.indexes[val]
+        insert_value = self.values[-1]
+        self.values[out_index] = insert_value
+        self.indexes[insert_value] = out_index
+        self.indexes.pop(val)
+        self.values.pop()
         return True
-        
+
     def getRandom(self) -> int:
-        return self.l[random.randint(0,len(self.l)-1)]
+        return random.choice(self.values)
 
 
 # Your RandomizedSet object will be instantiated and called as such:
