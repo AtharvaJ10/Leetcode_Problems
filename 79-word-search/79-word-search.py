19f6178432ef639138ff55a3416b49f04fa590ab
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-10 17:27:54 -0400

    Time: 4182 ms (91.30%), Space: 14 MB (12.49%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -1,22 +1,20 @@
-from collections import deque
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         def dfs(i,j,x):
-            if i<0 or i>=rows or j<0 or j>=cols or board[i][j]!=word[x] or x>=len(word) or visited[i][j]:
+            if x==len(word):
+                return True
+            if i<0 or i>=rows or j<0 or j>=cols or visited[i][j] or word[x]!=board[i][j]:
                 return False
             visited[i][j] = True
-            if x==len(word)-1:
-                return True
             res = dfs(i+1,j,x+1) or dfs(i,j-1,x+1) or dfs(i-1,j,x+1) or dfs(i,j+1,x+1)
             visited[i][j] = False
             return res
         
-        rows, cols, flag = len(board), len(board[0]), False
-        visited = [[False for _ in range(cols)] for _ in range (rows)]
+        rows, cols = len(board), len(board[0])
+        visited = [[False for _ in range(cols)] for _ in range(rows)]
         for i in range(rows):
             for j in range(cols):
-                if board[i][j]==word[0] and not visited[i][j]:
+                if not visited[i][j] and board[i][j]==word[0]:
                     if dfs(i,j,0):
                         return True
         return False
-                    
