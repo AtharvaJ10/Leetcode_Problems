28a6fc10a9f8b41a5fd16bbe0efe8c31f2032b23
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-09 21:20:27 -0400

    Time: 353 ms (60.11%), Space: 16.7 MB (66.01%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.py
+++ 1472-design-browser-history/1472-design-browser-history.py
@@ -1,30 +1,33 @@
+class Node:
+    def __init__(self, url):
+        self.url = url
+        self.next = self.prev = None
+        
 class BrowserHistory:
+
     def __init__(self, homepage: str):
-        self.arr = [homepage]
-        self.frwd = 0  # number of urls which will be displayed after passing forward
-        self.cur = 0 # current displayed url's index
+        self.history = Node(homepage)
 
     def visit(self, url: str) -> None:
-        self.arr = self.arr[:self.cur+1] # previous history(urls which will be displayed after forward) will be deleted
-        self.arr.append(url)
-        self.frwd = 0  # there is no forward history and set to 0
-        self.cur += 1 # current index is increased
-        
+        self.history.next = Node(url)
+        self.history.next.prev = self.history
+        self.history = self.history.next
 
-    def back(self, n: int) -> str:
-        if n <= self.cur: # switch to the page which is n steps back 
-            self.cur -= n 
-            self.frwd += n
-        else:
-            self.cur = 0 # current index is set to homepage
-            self.frwd = len(self.arr)-1  # the number of remaining pages for which there is access to the switch
-        return self.arr[self.cur] # returns the current page
+    def back(self, steps: int) -> str:
+        while steps and self.history.prev:
+            self.history = self.history.prev
+            steps-=1
+        return self.history.url
 
-    def forward(self, n: int) -> str:
-        if n <=self.frwd: # switch to the page which is n steps forward 
-            self.frwd -= n 
-            self.cur += n 
-        else:    # switch to the last page when n is greater than possible forward steps
-            self.cur = len(self.arr)-1 # current index is set to last page's index
-            self.frwd = 0 # there is no else forward page
-        return self.arr[self.cur] # returns the current page
+    def forward(self, steps: int) -> str:
+        while steps and self.history.next:
+            self.history = self.history.next
+            steps-=1
+        return self.history.url
+
+
+# Your BrowserHistory object will be instantiated and called as such:
+# obj = BrowserHistory(homepage)
+# obj.visit(url)
+# param_2 = obj.back(steps)
+# param_3 = obj.forward(steps)
