d799eade52a37c443d7ce9d914068e3b8ef9a99c
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-10 17:08:06 -0400

    Time: 99 ms (96.14%), Space: 15.7 MB (79.57%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
@@ -1,19 +1,13 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        if not graph:
-            return None
-        
-        d = {}
-        for i in range(len(graph)):
-            d[i] = graph[i]
-        
-        stack = [[0, [0]]]
+        stack = [(0, [0])]  #Stack of Node and path
         res = []
         while stack:
             node, path = stack.pop()
-            if node == len(graph)-1:
+            if node == len(graph)-1:   #if node is target
                 res.append(path)
             
-            for i in d[node]:
-                stack.append([i, path+[i]])
+            for neighbor in graph[node]:  # Add neighbors and respective path in stack
+                stack.append((neighbor, path+[neighbor]))
         return res
+            
