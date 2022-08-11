727da16d9031d5fc871defde9055ef5b409189da
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-11 17:02:29 -0400

    Time: 549 ms (73.81%), Space: 60.9 MB (97.73%) - LeetHub

--- 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.py
@@ -6,19 +6,19 @@ class RandomizedSet:
         self.indexes = defaultdict(int)
 
     def insert(self, val: int) -> bool:
-        if val not in self.indexes:
-            self.values.append(val)
-            self.indexes[val] = len(self.values)-1
-            return True
-        return False
+        if val in self.indexes:
+            return False
+        self.values.append(val)
+        self.indexes[val] = len(self.values)-1
+        return True
 
     def remove(self, val: int) -> bool:
         if val not in self.indexes:
             return False
         out_index = self.indexes[val]
-        insert_value = self.values[-1]
-        self.values[out_index] = insert_value
-        self.indexes[insert_value] = out_index
+        insert_val = self.values[-1]
+        self.indexes[insert_val] = out_index
+        self.values[out_index] = insert_val
         self.indexes.pop(val)
         self.values.pop()
         return True
