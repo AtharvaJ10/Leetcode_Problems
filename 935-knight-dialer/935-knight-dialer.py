943d9b2794f20d8a633cfdd5cdf8c95eedc41255
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-12 14:32:50 -0400

    Time: 4689 ms (17.83%), Space: 44.2 MB (20.86%) - LeetHub

--- 935-knight-dialer/935-knight-dialer.py
+++ 935-knight-dialer/935-knight-dialer.py
@@ -1,20 +1,18 @@
 class Solution:
     def knightDialer(self, n: int) -> int:
-        paths = {-1:[0,1,2,3,4,5,6,7,8,9], 0:[4,6], 1:[6,8], 2:[7,9], 3:[4,8], 4:[0,3,9], 5:[], 6:[0,1,7], 7:[2,6], 8:[1,3], 9:[2,4]}
-        return self.helper(paths, n, -1, {}) % (10**9 + 7)
+        paths = {-1:[0,1,2,3,4,5,6,7,8,9], 0: [4,6], 1: [6,8], 2: [7,9], 3: [4,8], 4:[0,3,9], 5:[], 6:[0,1,7], 7:[2,6], 8:[1,3], 9:[2,4]}
+        return self.helper(paths, n, -1, {}) % ((10**9)+7)
     
-    def helper(self, paths, steps, current, cache):
-        if (steps, current) in cache:
-            return cache[(steps, current)]
+    def helper(self, paths, steps, curr, cache):
+        if (steps, curr) in cache:
+            return cache[(steps, curr)]
         
-        if steps ==0:
+        if steps==0:
             return 1
         
         count = 0
-        for num in paths[current]:
+        for num in paths[curr]:
             count+=self.helper(paths, steps-1, num, cache)
         
-        cache[(steps, current)] = count
+        cache[(steps, curr)] = count
         return count
-        
-        
