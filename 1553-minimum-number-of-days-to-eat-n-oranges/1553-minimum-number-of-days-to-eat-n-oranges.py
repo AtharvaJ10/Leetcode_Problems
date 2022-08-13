1d434128c425e3bae9f830877e6985cdd4146882
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-13 00:00:42 -0400

    Time: 54 ms (82.15%), Space: 14.6 MB (42.79%) - LeetHub

--- 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.py
+++ 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.py
@@ -1,12 +1,10 @@
 class Solution:
     def minDays(self, n: int) -> int:
-        # O(log n)
-        dp = {0:0, 1:1}
+        dp = {0: 0, 1: 1}
         def dfs(n):
             if n in dp:
                 return dp[n]
             
-            # 1 day to divide and min of number of days to be perfectly divide by 2(n%2) + divide n//2, similarly for 3
-            dp[n] = 1+min(n%2 + dfs(n//2), n%3 + dfs(n//3))
+            dp[n] = 1 + min(n%2 + dfs(n//2), n%3 + dfs(n//3))
             return dp[n]
         return dfs(n)
