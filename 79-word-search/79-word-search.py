019a9923b43dff321d5b45ff78f8f112b07ee0c7
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-14 22:26:40 -0400

    Time: 7287 ms (56.49%), Space: 14.1 MB (12.49%) - LeetHub

--- 79-word-search/79-word-search.py
+++ 79-word-search/79-word-search.py
@@ -3,8 +3,10 @@ class Solution:
         def dfs(i,j,x):
             if x==len(word):
                 return True
-            if i<0 or i>=rows or j<0 or j>=cols or visited[i][j] or word[x]!=board[i][j]:
+            
+            if i<0 or i>=rows or j<0 or j>=cols or visited[i][j] or board[i][j]!=word[x]:
                 return False
+            
             visited[i][j] = True
             res = dfs(i+1,j,x+1) or dfs(i,j-1,x+1) or dfs(i-1,j,x+1) or dfs(i,j+1,x+1)
             visited[i][j] = False
@@ -12,9 +14,10 @@ class Solution:
         
         rows, cols = len(board), len(board[0])
         visited = [[False for _ in range(cols)] for _ in range(rows)]
+        x= 0
         for i in range(rows):
             for j in range(cols):
-                if not visited[i][j] and board[i][j]==word[0]:
-                    if dfs(i,j,0):
+                if not visited[i][j] and board[i][j]==word[x]:
+                    if dfs(i,j,x):
                         return True
         return False
