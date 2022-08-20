c2b4c25f9fabbaa40f936de3ceca70406003b788
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-20 11:25:59 -0400

    Time: 28 ms (98.91%), Space: 14 MB (29.05%) - LeetHub

--- 767-reorganize-string/767-reorganize-string.py
+++ 767-reorganize-string/767-reorganize-string.py
@@ -1,18 +1,19 @@
 import heapq
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        heap,res, d = [],"", Counter(s)
+        heap, res, d = [], "", Counter(s)
         for i in d:
-            heap.append([-d[i],i])
+            heap.append((-d[i], i))
+        
         heapq.heapify(heap)
-        
-        prev_key, prev_value = 0,0
+        prev_val, prev_key = 0, ""
         while heap:
-            curr_value, curr_key = heapq.heappop(heap)
-            if prev_value<0:
-                heapq.heappush(heap,[prev_value, prev_key])
+            curr_val, curr_key = heapq.heappop(heap)
+            if prev_val<0:
+                heapq.heappush(heap, (prev_val, prev_key))
             res+=curr_key
-            curr_value+=1
-            prev_value, prev_key = curr_value, curr_key
-        
+            curr_val+=1
+            prev_key, prev_val = curr_key, curr_val
+            
         return res if len(res)==len(s) else ""
+            
