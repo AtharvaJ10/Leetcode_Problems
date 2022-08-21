4bc5c3c5360873d5c20fe60cfe6b3e9f6cff654b
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-21 09:19:45 -0400

    Time: 1220 ms (38.59%), Space: 23.9 MB (53.71%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,52 +1,22 @@
+from collections import deque
 class Solution:
-    def longestSubarray(self, A: List[int], limit: int) -> int:
-        # This is a monotonically decreasing double-ended queue. 
-        maxd = collections.deque()
-
-        # This is a monotonically increasing double-ended queue.
-        mind = collections.deque()
-
+    def longestSubarray(self, nums: List[int], limit: int) -> int:
+        maxd = deque([])
+        mind = deque([])
         i = 0
-        for j in range(len(A)):
-            # At each iteration, we maintain the biggest elements in maxd.
-            # Remove any element smaller than A[j]
-            while len(maxd) and A[j] > maxd[-1]: maxd.pop()
-
-            # At each iteration, we maintain the smallest elements in mind.
-            # Remove any element bigger than A[j]
-            while len(mind) and A[j] < mind[-1]: mind.pop()
-
-            # Why do we always add A[j] ?
-            # As we will see below, we may have to remove an element(may be A[j-1] if i was j-1) 
-            # from the beginning of the maxd/mind.
-            # After that, we still need to know the max/min numbers from A[i/i+1]...A[j]
-            maxd.append(A[j])
-            mind.append(A[j])
-
-            # maxd holds the biggest elements from A[i]...A[j] in decreasing order.
-            # So maxd[0] is the biggest element in the window A[i]...A[j]
-            # mind holds the smallest elements from A[i]...A[j] in increasing order.
-            # So mind[0] is the smallest element in the window A[i]...A[j]
-            # maxd[0]-mind[0] is the biggest difference in the window A[i]...A[j]
-            if maxd[0] - mind[0] > limit:
-                # The biggest difference is over the limit; so remove A[i] from the window.
-                # Why do we check only maxd[0]/mind[0] to remove A[i]?
-                # Take maxd as an example. In order for A[i] to be present in maxd, 
-                # A[i] >= A[x], where x = i+1...j. In other words, it has to be the biggest element or 
-                # it would have already been removed. The biggest element would be in maxd[0]. 
-                # Similar explanation applies for mind.
-                if maxd[0] == A[i]: maxd.popleft()
-                if mind[0] == A[i]: mind.popleft()
-                # The new window for consideration is A[i+1]...A[j].
-                i += 1
-
-        # At every iteration of j, the window size for consideration is from A[i..j]. Its size is j+1-i.
-        # At every iteration, an element is added to the window and possibly removed only if the window contains
-        # elements with max difference > limit.
-        # So the window size only grows monotonically but never shrinks in size. The window grows only if all the elements in
-        # the window satisfy the max difference <= limit.
-        # Therefore, the last window size in the iteration(when j=len(A)-1) holds the maximum size of the window with max diff <= limit.
-        # However, it must be noted that the window in consideration at the last iteration may not really be the window
-        # which has the max diff <= limit.
-        # This doesn't matter since all we are interested in is the window size and not really the elements in the window.
-        return len(A) - i
+        for j in range(len(nums)):
+            while maxd and nums[maxd[-1]]<nums[j]:
+                maxd.pop()
+            maxd.append(j)
+            
+            while mind and nums[mind[-1]]>nums[j]:
+                mind.pop()
+            mind.append(j)
+            
+            if nums[maxd[0]]-nums[mind[0]]>limit:
+                if nums[maxd[0]]==nums[i]:
+                    maxd.popleft()
+                if nums[mind[0]]==nums[i]:
+                    mind.popleft()
+                i+=1
+        return len(nums)-i
