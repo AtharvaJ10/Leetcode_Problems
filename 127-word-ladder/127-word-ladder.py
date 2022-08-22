7dddf1dfa6384672bb6ad9ec7ab0a3e349c7be27
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-22 12:47:33 -0400

    Time: 765 ms (27.49%), Space: 15 MB (93.89%) - LeetHub

--- 127-word-ladder/127-word-ladder.py
+++ 127-word-ladder/127-word-ladder.py
@@ -2,9 +2,9 @@ from collections import deque
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         queue = deque([])
-        queue.append((beginWord, 1))
-        words = set(wordList)
+        queue.append([beginWord, 1])
         letters = 'abcdefghijklmnopqrstuvwxyz'
+        words = set(wordList)
         while queue:
             word, length = queue.popleft()
             if word==endWord:
@@ -13,6 +13,6 @@ class Solution:
                 for c in letters:
                     temp = word[:i] + c + word[i+1:]
                     if temp in words:
-                        words.discard(temp)
-                        queue.append((temp, length+1))
+                        words.remove(temp)
+                        queue.append([temp, length+1])
         return 0
