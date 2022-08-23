f09471820a6615cb37ca5822e067623820aceae3
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-23 17:49:38 -0400

    Time: 3092 ms (59.51%), Space: 53.4 MB (56.44%) - LeetHub

--- 2055-plates-between-candles/2055-plates-between-candles.py
+++ 2055-plates-between-candles/2055-plates-between-candles.py
@@ -1,37 +1,35 @@
 class Solution:
     def platesBetweenCandles(self, s: str, queries: List[List[int]]) -> List[int]:
-        nearest_right = []
-        nearest_left = []
+        n = len(s)
+        left = [-1]*n
         candles = []
-        
-        left, count = -1, 0
+        temp, count = -1,0
         for i in range(len(s)):
             if s[i]=='|':
-                left = i
                 count+=1
-            nearest_left.append(left)
+                temp = i
             candles.append(count)
-            
-        right = -1
-        for i in range(len(s)-1,-1,-1):
-            if s[i]=='|':
-                right = i
-            nearest_right.append(right)
-        nearest_right = nearest_right[::-1]
+            left[i] = temp
         
-        ans = [0]*len(queries)
-        for i in range(len(ans)):
-            start = queries[i][0]
-            end = queries[i][1]
-            
-            left , right = nearest_right[start], nearest_left[end]
-            if left ==-1 or right == -1:
-                ans[i] = 0
+        right = [-1]*n
+        temp = -1
+        for i in range(n-1,-1,-1):
+            if s[i]=='|':
+                temp = i
+            right[i] = temp
+        print(right)
+        
+        res = [0]*len(queries)
+        for i in range(len(queries)):
+            l,r = queries[i][0], queries[i][1]
+            nl, nr = right[l], left[r]
+            if nl==-1 or nr==-1:
+                res[i]=0
             else:
-                dist = right - left
-                if dist > 0:
-                    ans[i] = right - left + 1 - (candles[right] - candles[left] +1)
-                    # The count would be difference btn right and left nearest candle - number of candles in between.
+                dist = nr-nl
+                if dist<0:
+                    res[i] = 0
                 else:
-                    ans[i] = 0
-        return ans
+                    res[i] = (nr-nl+1) - (candles[nr]-candles[nl]+1)
+        return res
+                
