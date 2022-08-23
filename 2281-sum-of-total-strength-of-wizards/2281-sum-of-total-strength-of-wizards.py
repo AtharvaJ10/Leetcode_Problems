d732d574be62ebb489aefec871c613c261bba5d2
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-23 16:46:38 -0400

    Time: 2854 ms (24.54%), Space: 34.5 MB (54.66%) - LeetHub

--- 2281-sum-of-total-strength-of-wizards/2281-sum-of-total-strength-of-wizards.py
+++ 2281-sum-of-total-strength-of-wizards/2281-sum-of-total-strength-of-wizards.py
@@ -1,36 +1,33 @@
 class Solution:
-    def totalStrength(self, strength: List[int]) -> int:
-        n = len(strength)
+    def totalStrength(self, A: List[int]) -> int:
+        n = len(A)
         right = [n]*n
         stack = []
-        for i in range(n):
-            while stack and strength[stack[-1]]>strength[i]:
+        for i in range(len(A)):
+            while stack and A[stack[-1]]>A[i]:
                 right[stack.pop()] = i
             stack.append(i)
-        
+            
         left = [-1]*n
         stack = []
-        for i in range(n-1,-1,-1):
-            while stack and strength[stack[-1]]>=strength[i]:
+        for i in range(len(A)-1,-1,-1):
+            while stack and A[stack[-1]]>=A[i]:
                 left[stack.pop()] = i
             stack.append(i)
         
-        prefix_sum = [0]
-        for i in range(n):
-            prefix_sum.append(prefix_sum[-1]+strength[i])
+        prefix = [0]
+        for i in A:
+            prefix.append(prefix[-1]+i)
         
-        for i in range(1,n+1):
-            prefix_sum[i] = prefix_sum[i]+prefix_sum[i-1]
-
+        for i in range(1, len(prefix)):
+            prefix[i]+=prefix[i-1]
+        
         res = 0
-        for i in range(n):
+        for i in range(len(A)):
             l,r = left[i], right[i]
-            l_sum = prefix_sum[i] - prefix_sum[max(l,0)]
-            r_sum = prefix_sum[r] - prefix_sum[i]
-            l_range = i-l
-            r_range = r-i
-            res+= strength[i]* (r_sum*l_range - l_sum * r_range)
-        return res %(10**9 + 7)
-        
-        
-        
+            l_range, r_range = i-l, r-i
+            lsum = prefix[i] - prefix[max(l,0)]
+            rsum = prefix[r] - prefix[i]
+            res+=A[i]*(rsum*l_range - lsum*r_range)
+        return res % (10**9 + 7)
+                      
