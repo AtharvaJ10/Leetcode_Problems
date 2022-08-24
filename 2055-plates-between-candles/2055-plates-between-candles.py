f4236028a61014288c70b842699ebbb5c7c341e1
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-24 17:48:50 -0400

    Time: 2315 ms (86.06%), Space: 53.4 MB (56.44%) - LeetHub

--- 2055-plates-between-candles/2055-plates-between-candles.py
+++ 2055-plates-between-candles/2055-plates-between-candles.py
@@ -2,34 +2,33 @@ class Solution:
     def platesBetweenCandles(self, s: str, queries: List[List[int]]) -> List[int]:
         n = len(s)
         left = [-1]*n
-        candles = []
-        temp, count = -1,0
-        for i in range(len(s)):
+        candles =[]
+        count, c = 0,-1
+        for i in range(n):
             if s[i]=='|':
+                c = i
                 count+=1
-                temp = i
+            left[i] = c
             candles.append(count)
-            left[i] = temp
         
         right = [-1]*n
-        temp = -1
+        c = -1
         for i in range(n-1,-1,-1):
             if s[i]=='|':
-                temp = i
-            right[i] = temp
-        print(right)
+                c = i
+            right[i] = c
         
-        res = [0]*len(queries)
-        for i in range(len(queries)):
-            l,r = queries[i][0], queries[i][1]
-            nl, nr = right[l], left[r]
-            if nl==-1 or nr==-1:
-                res[i]=0
+        res=[]
+        for i,j in queries:
+            start = right[i]
+            end = left[j]
+            if start==-1 or end==-1:
+                res.append(0)
             else:
-                dist = nr-nl
-                if dist<0:
-                    res[i] = 0
+                dist = end - start
+                if dist>0:
+                    temp = end-start+1 - (candles[end] - candles[start]+1)
+                    res.append(temp)
                 else:
-                    res[i] = (nr-nl+1) - (candles[nr]-candles[nl]+1)
+                    res.append(0)
         return res
-                
