99eeaeb60edcf328f493e0a5400891b16819181e
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-24 14:31:47 -0400

    Time: 182 ms (35.85%), Space: 19.9 MB (9.21%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -2,6 +2,9 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         d = defaultdict(list)
         for i in strs:
-            d[tuple(sorted(i))].append(i)
+            count = [0 for i in range(26)]
+            for j in i:
+                count[ord(j) - ord('a')]+=1
+            d[tuple(count)].append(i)
         return d.values()
         
