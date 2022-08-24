97df9183760657519081f4514a1ac1e85d8056f2
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-24 17:33:11 -0400

    Time: 95 ms (91.94%), Space: 14.1 MB (37.69%) - LeetHub

--- 2193-minimum-number-of-moves-to-make-palindrome/2193-minimum-number-of-moves-to-make-palindrome.py
+++ 2193-minimum-number-of-moves-to-make-palindrome/2193-minimum-number-of-moves-to-make-palindrome.py
@@ -1,7 +1,7 @@
 class Solution:
     def minMovesToMakePalindrome(self, s: str) -> int:
-        res = 0
         s = list(s)
+        res = 0
         while s:
             index = s.index(s[-1])
             if index == len(s)-1:
