5223483bef6a4acf801ba150b6ef4e070fb65925
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-26 15:45:50 -0400

    Time: 149 ms (88.69%), Space: 16.8 MB (44.45%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -1,21 +1,18 @@
-from collections import deque
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        def dfs(i,j):
+            if i<0 or i>=rows or j<0 or j>=cols or visited[i][j] or grid[i][j]==0:
+                return 0
+            
+            visited[i][j] = True
+            res = 1 + dfs(i+1,j) + dfs(i,j-1) + dfs(i-1,j) + dfs(i,j+1)
+            return res
+        
         rows, cols = len(grid), len(grid[0])
-        current, max_area = 0,0
+        visited = [[False for _ in range(cols)] for _ in range(rows)]
+        res = 0
         for i in range(rows):
             for j in range(cols):
-                if grid[i][j]==1:
-                    queue = deque([])
-                    queue.append([i,j])
-                    current = 1
-                    grid[i][j] = 0
-                    while queue:
-                        k,l = queue.popleft()
-                        for x,y in [k+1,l], [k,l-1], [k-1,l], [k,l+1]:
-                            if 0<=x<rows and 0<=y<cols and grid[x][y]==1:
-                                grid[x][y] = 0
-                                queue.append([x,y])
-                                current+=1
-                    max_area = max(current, max_area)
-        return max_area
+                if not visited[i][j] and grid[i][j]==1:
+                    res = max(res, dfs(i,j))
+        return res
