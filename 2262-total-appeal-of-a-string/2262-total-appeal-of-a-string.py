12ddffa39ab0572bbc88a9257887faf705bb17af
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-27 17:29:07 -0400

    Time: 259 ms (72.98%), Space: 14.8 MB (96.16%) - LeetHub

--- 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.py
+++ 2262-total-appeal-of-a-string/2262-total-appeal-of-a-string.py
@@ -1,9 +1,10 @@
 class Solution:
     def appealSum(self, s: str) -> int:
-        last = {c:-1 for c in ascii_lowercase}
+        last = defaultdict(lambda:-1)
         res = 0
         for i,c in enumerate(s):
             j = last[c]
             res+=(len(s)-i)*(i-j)
             last[c] = i
         return res
+        
