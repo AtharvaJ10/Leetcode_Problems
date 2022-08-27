8e8e9e07cfaccaf3b3161e3be668a4b5f05d14b9
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-27 16:52:17 -0400

    Time: 885 ms (73.12%), Space: 24 MB (31.77%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -3,20 +3,20 @@ class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
         maxd = deque([])
         mind = deque([])
-        i = 0
-        for j in range(len(nums)):
-            while maxd and nums[maxd[-1]]<nums[j]:
+        j = 0
+        for i in range(len(nums)):
+            while maxd and nums[maxd[-1]]<nums[i]:
                 maxd.pop()
-            maxd.append(j)
+            maxd.append(i)
             
-            while mind and nums[mind[-1]]>nums[j]:
+            while mind and nums[mind[-1]]>nums[i]:
                 mind.pop()
-            mind.append(j)
+            mind.append(i)
             
             if nums[maxd[0]]-nums[mind[0]]>limit:
-                if nums[maxd[0]]==nums[i]:
+                if nums[maxd[0]]==nums[j]:
                     maxd.popleft()
-                if nums[mind[0]]==nums[i]:
+                if nums[mind[0]]==nums[j]:
                     mind.popleft()
-                i+=1
-        return len(nums)-i
+                j+=1
+        return len(nums)-j
