95c13a805f1e93f283ef9350e4062ea64106d838
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-27 17:15:58 -0400

    Time: 53 ms (48.92%), Space: 14.2 MB (12.06%) - LeetHub

--- 273-integer-to-english-words/273-integer-to-english-words.py
+++ 273-integer-to-english-words/273-integer-to-english-words.py
@@ -1,8 +1,8 @@
 class Solution:
     def numberToWords(self, num: int) -> str:
         till19 = "One Two Three Four Five Six Seven Eight Nine Ten Eleven Twelve Thirteen Fourteen Fifteen Sixteen Seventeen Eighteen Nineteen".split(" ")
-        tens = 'Twenty Thirty Forty Fifty Sixty Seventy Eighty Ninety'.split(" ")
-        bigs = {1000000000: "Billion", 1000000: "Million", 1000: "Thousand"}
+        tens = "Twenty Thirty Forty Fifty Sixty Seventy Eighty Ninety".split(" ")
+        bigs = {1000000000: "Billion", 1000000: "Million", 1000:"Thousand"}
         
         def word(n):
             if n<20:
@@ -13,6 +13,7 @@ class Solution:
                 return [till19[n//100-1]] + ['Hundred'] + word(n%100)
             else:
                 for i in [1000000000, 1000000, 1000]:
-                    if n//i>0:
+                    if n//i:
                         return word(n//i) + [bigs[i]] + word(n%i)
-        return ' '.join(word(num)) if num else 'Zero'
+        
+        return ' '.join([i for i in word(num)]) if num else "Zero"
