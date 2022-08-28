0d5a3a4b6b6164f8165c1060e2bf4bb5a1c90995
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-28 12:03:08 -0400

    Time: 31 ms (95.19%), Space: 13.9 MB (95.24%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.py
+++ 399-evaluate-division/399-evaluate-division.py
@@ -1,51 +1,28 @@
-from collections import defaultdict
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         def bfs(start, dest):
-            if start not in graph and dest not in graph:
-                return -1.0
-            queue = [(start, 1.0)]
-            visited = set()
-            for x,v in queue:
-                if x==dest:
-                    return v
-                visited.add(x)
-                for j in graph[x]:
-                    if j not in visited:
-                        queue.append([j, v*graph[x][j]])
-            return -1.0
+            if start not in graph or dest not in graph:
+                return -1
+            queue = deque([])
+            queue.append([start, 1.0])
+            visited = set([start])
+            while queue:
+                node, val = queue.popleft()
+                if node==dest:
+                    return val
+                for i in graph[node]:
+                    if i not in visited:
+                        visited.add(i)
+                        queue.append([i, val*graph[node][i]])
+            return -1
+        
         
         graph = defaultdict(dict)
-        for eq, val in zip(equations, values):
-            i,j = eq
-            graph[i][j] = val
-            graph[j][i] = 1/val
-        
-        return [bfs(i,j) for i,j in queries]
-        
-        """def dfs(graph, start, end, visited):
-            if start == end and graph[start]:
-                return 1.0
+        for eq,v in zip(equations, values):
+            graph[eq[0]][eq[1]] = v
+            graph[eq[1]][eq[0]] = 1/v
             
-            visited.add(start)
-            for neigh, val in graph[start]:
-                if neigh in visited:
-                    continue
-                
-                tmp = dfs(graph, neigh, end, visited)
-                if tmp > 0:
-                    return val * tmp
-            
-            return -1.0
-            
-        graph = collections.defaultdict(set)
-        for items, v in zip(equations, values):
-            x, y = items
-            graph[x].add((y, v))
-            graph[y].add((x, 1.0 / v))
-        
         res = []
-        for q in queries:
-            res.append(dfs(graph, q[0], q[1], set()))
-        
-        return res"""
+        for i,j in queries:
+            res.append(bfs(i,j))
+        return res
