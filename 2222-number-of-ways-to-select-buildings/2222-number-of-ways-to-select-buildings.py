832e537bce3fbb90b5ee5f96552642ceab1cadd6
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-28 12:22:21 -0400

    Time: 1068 ms (63.00%), Space: 15.2 MB (43.14%) - LeetHub

--- 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.py
+++ 2222-number-of-ways-to-select-buildings/2222-number-of-ways-to-select-buildings.py
@@ -1,21 +1,20 @@
 class Solution:
     def numberOfWays(self, s: str) -> int:
         a,b,res = 0,0,0
-        for i in range(len(s)):
-            if s[i]=="1":
+        for i in s:
+            if i=="1":
                 a+=1
             else:
                 b+=1
         
         c,d = 0,0
-        for i in range(len(s)):
-            if s[i]=="1":
-                res+= (b*d)
+        for i in s:
+            if i=="1":
+                res+=(b*d)
                 a-=1
                 c+=1
             else:
                 res+=(a*c)
                 b-=1
                 d+=1
-        
         return res
