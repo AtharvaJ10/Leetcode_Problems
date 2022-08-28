a88e110a64cac29356672343c38cc90e7ac4abed
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-28 11:48:49 -0400

    Time: 2090 ms (40.91%), Space: 31 MB (22.19%) - LeetHub

--- 1856-maximum-subarray-min-product/1856-maximum-subarray-min-product.py
+++ 1856-maximum-subarray-min-product/1856-maximum-subarray-min-product.py
@@ -2,26 +2,27 @@ class Solution:
     def maxSumMinProduct(self, nums: List[int]) -> int:
         n = len(nums)
         right = [n]*n
-        stack =[]
+        stack = []
         for i in range(len(nums)):
             while stack and nums[stack[-1]]>nums[i]:
-                right[stack.pop()]=i
+                right[stack.pop()] = i
             stack.append(i)
         
         left = [-1]*n
         stack = []
         for i in range(n-1,-1,-1):
             while stack and nums[stack[-1]]>=nums[i]:
-                left[stack.pop()]=i
+                left[stack.pop()] = i
             stack.append(i)
         
         pre = [0]
         for i in nums:
-            pre.append(i+pre[-1])
-        
+            pre.append(pre[-1]+i)
+            
         res = 0
         for i in range(len(nums)):
             l,r = left[i], right[i]
-            sum1 = pre[r] - pre[max(l+1,0)]
-            res = max(res, nums[i]*sum1)
-        return res % (10**9 + 7)
+            res= max(res, nums[i]*(pre[r]-pre[l+1]))
+            
+        return res%(10**9 + 7)
+        
