d5b2641968e0fe73f4f19662275fc2f3d440207f
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-28 12:14:34 -0400

    Time: 1147 ms (86.10%), Space: 33.6 MB (28.25%) - LeetHub

--- 2100-find-good-days-to-rob-the-bank/2100-find-good-days-to-rob-the-bank.py
+++ 2100-find-good-days-to-rob-the-bank/2100-find-good-days-to-rob-the-bank.py
@@ -1,26 +1,21 @@
 class Solution:
     def goodDaysToRobBank(self, security: List[int], time: int) -> List[int]:
-        if time==0:
-            return list(range(len(security)))
-        
         left = [0]
-        temp = 0
-        for i in range(1, len(security)):
+        l,n = 0, len(security)
+        for i in range(1, n):
             if security[i]<=security[i-1]:
-                temp+=1
+                l+=1
             else:
-                temp = 0
-            left.append(temp)
-        
-        right = [0]
-        temp = 0
-        for i in range(len(security)-2,-1,-1):
+                l = 0
+            left.append(l)
+            
+        right = [0]*n
+        r = 0
+        for i in range(n-2,-1,-1):
             if security[i]<=security[i+1]:
-                temp+=1
+                r+=1
             else:
-                temp = 0
-            right.append(temp)
-        right = right[::-1]
+                r = 0
+            right[i] = r
         
-        res=[i for i in range(len(security)) if left[i]>=time and right[i]>=time]
-        return res
+        return [i for i in range(n) if left[i]>=time and right[i]>=time]
