05a889372149b60d47a3d7a8059803bdd4a67ff7
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-28 14:40:01 -0400

    Time: 4479 ms (8.36%), Space: 62.1 MB (72.38%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.py
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.py
@@ -1,19 +1,18 @@
-import heapq
 class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
+        tasks = sorted([tasks[i][0], tasks[i][1], i] for i in range(len(tasks)))
         heap = []
-        tasks = sorted([(tasks[i][0], tasks[i][1],i) for i in range(len(tasks))])
         time = tasks[0][0]
-        i = 0
         res = []
-        while len(res)<len(tasks):
+        i=0
+        while len(res)!=len(tasks):
             while i<len(tasks) and tasks[i][0]<=time:
-                heapq.heappush(heap, [tasks[i][1], tasks[i][2]])
+                heapq.heappush(heap, (tasks[i][1], tasks[i][2]))
                 i+=1
             if heap:
-                t_diff, index = heapq.heappop(heap)
-                time+=t_diff
-                res.append(index)
-            elif i<len(tasks):
+                diff, ind = heapq.heappop(heap)
+                time+=diff
+                res.append(ind)
+            else:
                 time = tasks[i][0]
         return res
