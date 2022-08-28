d774692dd61794e24c9f4b2667509e94702ddfc3
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-28 11:25:05 -0400

    Time: 46 ms (57.02%), Space: 13.9 MB (79.34%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -3,16 +3,12 @@ class Solution:
         d = {2: 'abc', 3:'def', 4:'ghi', 5:'jkl', 6:'mno', 7:'pqrs', 8:'tuv', 9:'wxyz'}
         return self.helper(digits, 0, d, [], '')
     
-    def helper(self, digits, ind, d, res, curr):
+    def helper(self, digits, ind, d, res, path):
         if ind==len(digits):
-            res.append(curr)
-            curr = ''
+            res.append(path)
             return
         
         for i in d[int(digits[ind])]:
-            curr+=i
-            self.helper(digits, ind+1, d, res, curr)
-            curr = curr[:-1]
-        
+            self.helper(digits, ind+1, d, res, path+i)        
         return res
             
