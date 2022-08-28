e1c57ed46a767dc7bf320e7e0c8f893451168724
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-28 09:58:01 -0400

    Time: 152 ms (81.34%), Space: 15 MB (50.28%) - LeetHub

--- 719-find-k-th-smallest-pair-distance/719-find-k-th-smallest-pair-distance.py
+++ 719-find-k-th-smallest-pair-distance/719-find-k-th-smallest-pair-distance.py
@@ -1,21 +1,22 @@
 class Solution:
     def smallestDistancePair(self, nums: List[int], k: int) -> int:
-        def enough(distance) -> bool:  # two pointers
-            count, i, j = 0, 0, 0
-            while i < n or j < n:
-                while j < n and nums[j] - nums[i] <= distance:
-                    j += 1
-                count += j - i - 1
-                i += 1
-            return count >= k
-
+        def enough(dist):
+            i,j,count = 0,0,0
+            while i<n or j<n:
+                while j<n and nums[j]-nums[i]<=dist:
+                    j+=1
+                count+=j-i-1
+                i+=1
+            return count>=k
+        
         nums.sort()
-        n = len(nums)
-        left, right = 0, nums[-1] - nums[0]
-        while left < right:
-            mid = left + (right - left) // 2
+        n, low ,hi = len(nums), 0, nums[-1]-nums[0]
+        while low<hi:
+            print(low, hi)
+            mid = (low+hi)//2
             if not enough(mid):
-                left = mid + 1
+                low = mid + 1
             else:
-                right = mid
-        return left
+                hi = mid
+            #print(low, hi)
+        return low
