f06a96c6d7f7c5448e8d17ce2664c143f527c1f3
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-27 21:35:44 -0400

    Time: 127 ms (88.44%), Space: 14 MB (31.73%) - LeetHub

--- 909-snakes-and-ladders/909-snakes-and-ladders.py
+++ 909-snakes-and-ladders/909-snakes-and-ladders.py
@@ -1,28 +1,27 @@
 from collections import deque
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
-        def compute(square):
-            r,c = divmod(square-1, len(board))
+        def get_index(square):
+            r,c = divmod(square-1, n)
             if r%2!=0:
                 return n-r-1, n-c-1
             else:
                 return n-r-1, c
         
+        n = len(board)
         queue = deque([])
         queue.append([1,0])
         visited = set()
-        n = len(board)
         while queue:
-            square, step = queue.popleft()
-            r,c = compute(square)
+            square, steps = queue.popleft()
+            r,c = get_index(square)
             if board[r][c]!=-1:
                 square = board[r][c]
-            if square == len(board)**2:
-                return step
-            for i in range(1, 7):
-                new_square= square + i
-                if new_square <= len(board)**2 and new_square not in visited:
-                    visited.add(new_square)
-                    queue.append([new_square, step+1])
-        return -1
+            if square==n*n:
+                return steps
         
+            for i in range(1,7):
+                if square+i<=n*n and square+i not in visited:
+                    visited.add(square+i)
+                    queue.append([square+i, steps+1])
+        return -1
