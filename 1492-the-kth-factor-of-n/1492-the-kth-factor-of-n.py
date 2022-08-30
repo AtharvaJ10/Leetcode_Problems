ae8daae5ef53b7354c1d0f3cb993ee73ba4581ee
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-30 12:44:18 -0400

    Time: 34 ms (91.54%), Space: 13.9 MB (15.85%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.py
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.py
@@ -9,3 +9,15 @@ class Solution:
             if factors[-1]==factors[-2]:
                 factors.pop()
         return factors[k-1] if k<=len(factors) else -1
+    
+        """isqrt = math.isqrt(n)
+        for i in range(1, isqrt + 1):
+            if n % i == 0:
+			    k -= 1
+                if k == 0: return i
+        for i in reversed(range(1, isqrt + 1)):
+            if i * i == n: continue
+            if n % i == 0:
+			    k -= 1
+                if k == 0: return n // i
+        return -1"""
