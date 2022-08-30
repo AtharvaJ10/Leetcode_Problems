0ff40f35146d41ffb6ab6a3067d2d79b3365f781
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-30 12:13:35 -0400

    Time: 57 ms (36.16%), Space: 13.9 MB (86.30%) - LeetHub

--- 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.py
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.py
@@ -1,22 +1,27 @@
 class Solution:
     def tictactoe(self, moves: List[List[int]]) -> str:
-        row = [[0 for _ in range(3)] for j in range(2)]
-        col = [[0 for _ in range(3)] for j in range(2)]
-        d1 = [0 for i in range(2)]
-        d2 = [0 for i in range(2)]
-        id = 0
-        for i in moves:
-            row[id][i[0]]+=1
-            col[id][i[1]]+=1
+        rows = [0]*3
+        cols = [0]*3
+        d1,d2 = 0,0
+        
+        for i in range(len(moves)):
+            if i%2==0:
+                rows[moves[i][0]]+=1
+                cols[moves[i][1]]+=1
+                if moves[i][0]==moves[i][1]:
+                    d1+=1
+                if moves[i][0]+moves[i][1]==2:
+                    d2+=1
+            else:
+                rows[moves[i][0]]-=1
+                cols[moves[i][1]]-=1
+                if moves[i][0]==moves[i][1]:
+                    d1-=1
+                if moves[i][0]+moves[i][1]==2:
+                    d2-=1
             
-            if i[0]+i[1] == 2:
-                d1[id]+=1
-            if i[0] == i[1]:
-                d2[id]+=1
-            
-            if 3 in (row[id][i[0]], col[id][i[1]], d1[id], d2[id]):
-                return "A" if id%2==0 else "B"
-            id+=1
-            id%=2
-        return 'Draw' if len(moves) == 9 else 'Pending'
-            
+            if 3 in (rows[moves[i][0]], cols[moves[i][1]], d1, d2):
+                return "A"
+            elif -3 in (rows[moves[i][0]], cols[moves[i][1]], d1, d2):
+                return "B"
+        return "Pending" if len(moves)<9 else "Draw"
