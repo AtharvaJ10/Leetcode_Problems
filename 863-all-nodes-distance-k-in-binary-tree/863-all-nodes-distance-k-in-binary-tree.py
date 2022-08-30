3a3727d6adce999ebdae771ce21102db197e07c7
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-30 11:43:41 -0400

    Time: 50 ms (70.49%), Space: 14.5 MB (27.70%) - LeetHub

--- 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.py
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.py
@@ -5,26 +5,25 @@
 #         self.left = None
 #         self.right = None
 
-from collections import defaultdict
 class Solution:
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
         def dfs(parent, child):
             if parent and child:
-                graph[parent.val].append(child.val)
-                graph[child.val].append(parent.val)
+                conn[parent.val].append(child.val)
+                conn[child.val].append(parent.val)
             if child.left:
                 dfs(child, child.left)
             if child.right:
                 dfs(child, child.right)
-                
-        graph = defaultdict(list)
+        
+        conn = defaultdict(list)
         dfs(None, root)
         queue = [target.val]
         visited = set(queue)
         for i in range(k):
             level = []
-            for j in queue:
-                for l in graph[j]:
+            for j in range(len(queue)):
+                for l in conn[queue[j]]:
                     if l not in visited:
                         level.append(l)
             queue = level
