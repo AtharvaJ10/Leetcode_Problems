621fee3a6aeda8f47d0f0545264bb7defe15a8b8
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-30 22:22:49 -0400

    Time: 112 ms (84.59%), Space: 13.9 MB (91.86%) - LeetHub

--- 2193-minimum-number-of-moves-to-make-palindrome/2193-minimum-number-of-moves-to-make-palindrome.py
+++ 2193-minimum-number-of-moves-to-make-palindrome/2193-minimum-number-of-moves-to-make-palindrome.py
@@ -1,33 +1,13 @@
 class Solution:
     def minMovesToMakePalindrome(self, s: str) -> int:
-        """l = list(s)
+        l = list(s)
         res = 0
         while l:
             index = l.index(l[-1])
-            if index == len(l)-1:
+            if index==len(l)-1:
                 res+=index//2
             else:
                 res+=index
                 l.pop(index)
             l.pop()
-        return res"""
-        
-        l, r, s = 0, len(s) - 1, list(s)
-        ans = 0
-        while l < r:
-            if s[l] != s[r]:
-                k = r
-                while k > l and s[k] != s[l]:
-                    k -= 1
-                if k == l:
-                    ans += 1
-                    s[l], s[l + 1] = s[l + 1], s[l]
-                else:
-                    while k < r:
-                        s[k], s[k + 1] = s[k + 1], s[k]
-                        k += 1
-                        ans += 1
-                    l, r = l + 1, r - 1
-            else:
-                l, r = l + 1, r - 1
-        return ans
+        return res
