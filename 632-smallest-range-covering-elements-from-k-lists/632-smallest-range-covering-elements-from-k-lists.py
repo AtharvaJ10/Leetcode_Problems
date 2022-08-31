fa378ecf67a307ebbf761524b0587383e17fe871
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-31 10:41:55 -0400

    Time: 322 ms (68.13%), Space: 20.4 MB (83.10%) - LeetHub

--- 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.py
+++ 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.py
@@ -3,15 +3,16 @@ class Solution:
     def smallestRange(self, nums: List[List[int]]) -> List[int]:
         heap = [(nums[i][0], i, 0) for i in range(len(nums))]
         heapq.heapify(heap)
-        ans = [-float('inf'), float('inf')]
-        right = max(nums[i][0] for i in range(len(nums)))
+        right = max([i[0] for i in heap])
+        res = (-float('inf'), float('inf'))
         while heap:
-            left, i, j = heapq.heappop(heap)
-            if right-left < ans[1]-ans[0]:
-                ans = [left, right]
-            if j+1 == len(nums[i]):
-                return ans
+            left,i,j = heapq.heappop(heap)
+            if right-left<res[1]-res[0]:
+                res = [left,right]
+            if j+1==len(nums[i]):
+                return res
             v = nums[i][j+1]
             right = max(right, v)
             heapq.heappush(heap, (v,i,j+1))
-        return ans
+        return res
+                  
