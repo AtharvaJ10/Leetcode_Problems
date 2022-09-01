f771a2efdc00cd4bf9c4763ff574a0eec32f4e46
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-31 23:38:53 -0400

    Time: 165 ms (30.72%), Space: 14 MB (92.74%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,16 +1,16 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         candidates.sort()
-        return self.helper(0, candidates, target, [], 0, "")
+        return self.helper(0, candidates, target, [], 0, [])
     
     def helper(self, index, candidates, target, res, cur, path):
         if cur>target:
             return
         if cur==target:
-            res.append(path.split(" ")[1:])
+            res.append(path)
             return
         
         for i in range(index, len(candidates)):
-            self.helper(i, candidates, target, res, cur+candidates[i], path+" "+str(candidates[i]))
+            self.helper(i, candidates, target, res, cur+candidates[i], path+[candidates[i]])
         
         return res
