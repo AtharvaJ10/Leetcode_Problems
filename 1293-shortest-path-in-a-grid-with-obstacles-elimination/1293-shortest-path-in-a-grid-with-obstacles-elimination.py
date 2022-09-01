98b1698d2bd2fe64be908f6cc50aa3b53412e176
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-09-01 12:55:07 -0400

    Time: 123 ms (71.65%), Space: 15.6 MB (54.64%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -1,6 +1,7 @@
 class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
         rows,cols = len(grid), len(grid[0])
+        if k >= rows + cols - 2: return rows + cols - 2  # if we can go by manhattan distance -> let's go
         queue = deque([])
         queue.append([0,0,k,0])
         visited = set()
