3db447e58309601f6032c1499719fe0508308e28
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-09-01 14:41:40 -0400

    Time: 442 ms (90.84%), Space: 61.1 MB (85.73%) - LeetHub

--- 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.py
@@ -3,25 +3,25 @@ class RandomizedSet:
 
     def __init__(self):
         self.values = []
-        self.indexes = defaultdict(int)
+        self.indices = defaultdict(int)
 
     def insert(self, val: int) -> bool:
-        if val in self.indexes:
-            return False
-        self.values.append(val)
-        self.indexes[val] = len(self.values)-1
-        return True
+        if val not in self.indices:
+            self.values.append(val)
+            self.indices[val] = len(self.values)-1
+            return True
+        return False
 
     def remove(self, val: int) -> bool:
-        if val not in self.indexes:
-            return False
-        out_index = self.indexes[val]
-        insert_val = self.values[-1]
-        self.indexes[insert_val] = out_index
-        self.values[out_index] = insert_val
-        self.indexes.pop(val)
-        self.values.pop()
-        return True
+        if val in self.indices:
+            out_index = self.indices[val]
+            insert_val = self.values[-1]
+            self.values[out_index] = insert_val
+            self.indices[insert_val] = out_index
+            del self.indices[val]
+            self.values.pop()
+            return True
+        return False
 
     def getRandom(self) -> int:
         return random.choice(self.values)
