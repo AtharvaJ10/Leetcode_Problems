ed4849899bdb5a7c2d4ce5d1c9afa5b5cce3f775
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-09-01 12:52:22 -0400

    Time: 1750 ms (9.02%), Space: 22.4 MB (16.28%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -1,26 +1,22 @@
 class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
-        m, n = len(grid), len(grid[0])
-        if k >= m + n - 2: return m + n - 2  # if we can go by manhattan distance -> let's go
-        
-        DIR = [0, 1, 0, -1, 0]
-        q = deque([(0, 0, k)])  # pair of (r, c)
-        seen = set()
-        seen.add((0, 0, k))
+        rows,cols = len(grid), len(grid[0])
+        queue = deque([])
+        queue.append([0,0,k])
         step = 0
-        while q:
-            for _ in range(len(q)):
-                r, c, k = q.popleft()
-                if r == m - 1 and c == n - 1: return step  # Reach to the bottom right cell
-                for i in range(4):
-                    nr, nc = r + DIR[i], c + DIR[i + 1]
-                    if nr < 0 or nr == m or nc < 0 or nc == n: continue  # Skip out of bound cells!
-                    newK = k - grid[nr][nc]
-                    newState = (nr, nc, newK)
-                    if newK >= 0 and newState not in seen:
-                        seen.add(newState)
-                        q.append(newState)
+        visited = set()
+        visited.add((0,0,k))
+        while queue:
+            for _ in range(len(queue)):
+                i,j,k = queue.popleft()
+                if (i,j)==(rows-1,cols-1):
+                    return step
 
-            step += 1
-
+                for x,y in [i+1,j], [i,j-1], [i-1,j], [i,j+1]:
+                    if 0<=x<rows and 0<=y<cols:
+                        new_k = k-grid[x][y]
+                        if new_k>=0 and (x,y,new_k) not in visited:
+                            visited.add((x,y,new_k))
+                            queue.append([x,y,new_k])
+            step+=1
         return -1
