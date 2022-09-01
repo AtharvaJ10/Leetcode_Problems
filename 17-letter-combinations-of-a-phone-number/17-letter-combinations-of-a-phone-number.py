2db0816713078548f45dfc75fd595c7d837ea867
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-08-31 23:29:43 -0400

    Time: 42 ms (68.56%), Space: 13.8 MB (79.15%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -1,14 +1,17 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        d = {'2':'abc', '3':'def','4':'ghi','5':'jkl','6':'mno','7':'pqrs','8':'tuv','9':'wxyz'}
-        return self.helper(0, digits, "", d, [])
-    
-    def helper(self, curr, digits, path, d, res):
-        if len(path)==len(digits):
-            res.append(path)
-            return
-    
-        for i in d[digits[curr]]:
-            self.helper(curr+1, digits, path+i, d, res)
-        
+        if not digits:
+            return []
+        d = {"2":'abc',"3":'def',"4":'ghi','5':'jkl','6':'mno','7':'pqrs','8':'tuv','9':'wxyz'}
+        res = []
+        self.helper(0, digits, d, "", res)
         return res
+    
+    def helper(self, index, digits, d, cur, res):
+        if len(cur)==len(digits):
+            res.append(cur)
+            return
+        
+        for i in d[digits[index]]:
+            self.helper(index+1, digits, d, cur+i, res)
+            
