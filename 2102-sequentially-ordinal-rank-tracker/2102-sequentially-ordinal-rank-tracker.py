43de2858ea0bdcf5f1fac91fe56037aa15cc4a19
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-09-02 19:21:25 -0400

    Time: 1457 ms (73.35%), Space: 40.9 MB (9.20%) - LeetHub

--- 2102-sequentially-ordinal-rank-tracker/2102-sequentially-ordinal-rank-tracker.py
+++ 2102-sequentially-ordinal-rank-tracker/2102-sequentially-ordinal-rank-tracker.py
@@ -1,27 +1,24 @@
-import heapq
-
-class h1Node:
+class h1:
     def __init__(self, name, score):
-        self.name = name
+        self.name= name
         self.score = score
-        
+    
     def __lt__(self, other):
         return self.score>other.score or (self.score==other.score and self.name<other.name)
     
-    def to_h2(self):
-        return h2Node(self.name, self.score)
-    
-class h2Node:
+    def toh2(self):
+        return h2(self.name, self.score)
+
+class h2:
     def __init__(self, name, score):
-        self.name = name
+        self.name= name
         self.score = score
-        
+    
     def __lt__(self, other):
         return self.score<other.score or (self.score==other.score and self.name>other.name)
     
-    def to_h1(self):
-        return h1Node(self.name, self.score)
-    
+    def toh1(self):
+        return h1(self.name, self.score)
     
 class SORTracker:
 
@@ -30,18 +27,18 @@ class SORTracker:
         self.min = []
 
     def add(self, name: str, score: int) -> None:
-        n1 = h1Node(name, score)
-        n2 = h2Node(name, score)
+        n1 = h1(name,score)
+        n2 = h2(name,score)
         if not self.min or self.min[0]>n2:
             heapq.heappush(self.max, n1)
         else:
             heapq.heappush(self.min, n2)
             node = heapq.heappop(self.min)
-            heapq.heappush(self.max, node.to_h1())
+            heapq.heappush(self.max, node.toh1())
 
     def get(self) -> str:
         node = heapq.heappop(self.max)
-        heapq.heappush(self.min, node.to_h2())
+        heapq.heappush(self.min, node.toh2())
         return node.name
 
 
