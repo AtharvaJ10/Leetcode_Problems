b9a9d9fd0e51d7ed145ef0aa9a41a948178856c0
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-09-03 09:42:28 -0400

    Time: 1835 ms (5.01%), Space: 15.4 MB (26.16%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -2,19 +2,16 @@ class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         if grid[0][0]==1:
             return -1
-        rows, cols = len(grid), len(grid[0])
         queue = deque([])
-        queue.append((0,0,1))
-        visited = [[False for _ in range(cols)] for _ in range(rows)]
-        visited[0][0] = True
+        queue.append([0,0,1])
+        n = len(grid)
+        visited = set((0,0))
         while queue:
-            i,j, steps = queue.popleft()
-            if (i,j) == (rows-1,cols-1):
-                return steps
-            for x,y in [i+1,j+1], [i+1,j], [i+1,j-1], [i,j-1], [i-1,j-1], [i-1,j], [i-1,j+1], [i,j+1]:
-                if 0<=x<rows and 0<=y<cols and grid[x][y]==0 and not visited[x][y]:
-                    if (x,y) == (rows-1,cols-1):
-                        return steps+1
-                    visited[x][y] = True
-                    queue.append((x,y,steps+1))
+            i,j,moves = queue.popleft()
+            if (i,j)==(n-1,n-1):
+                return moves
+            for x,y in [i+1,j], [i+1,j-1], [i,j-1], [i-1,j-1], [i-1,j], [i-1,j+1], [i,j+1], [i+1,j+1]:
+                if 0<=x<n and 0<=y<n and (x,y) not in visited and grid[x][y]==0: 
+                    visited.add((x,y))
+                    queue.append([x,y,moves+1])
         return -1
