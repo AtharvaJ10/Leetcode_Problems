47e1bc700a568db68eee15cb090d9d5cd83f9ab8
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-09-03 12:34:29 -0400

    Time: 65 ms (33.42%), Space: 14 MB (34.80%) - LeetHub

--- 979-distribute-coins-in-binary-tree/979-distribute-coins-in-binary-tree.py
+++ 979-distribute-coins-in-binary-tree/979-distribute-coins-in-binary-tree.py
@@ -6,14 +6,13 @@
 #         self.right = right
 class Solution:
     def distributeCoins(self, root: Optional[TreeNode]) -> int:
-        self.moves = 0
-        def dfs(root):
-            if not root:
+        self.ans = 0
+        def dfs(node):
+            if not node:
                 return 0
-            left = dfs(root.left)
-            right = dfs(root.right)
-            self.moves += abs(left) + abs(right)
-            return root.val + left + right -1
-        
+            left = dfs(node.left)
+            right = dfs(node.right)
+            self.ans += abs(left) + abs(right)
+            return node.val+left+right-1
         dfs(root)
-        return self.moves
+        return self.ans
