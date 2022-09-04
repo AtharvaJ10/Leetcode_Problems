381055410e85b5a7f46b046a93a994b5fe4b481a
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-09-03 23:08:55 -0400

    Time: 125 ms (72.54%), Space: 13.8 MB (82.67%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -2,7 +2,7 @@ class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         set1 = set()
         for i in range(len(board)):
-            for j in range(len(board[0])):
+            for j in range(len(board)):
                 if board[i][j]!='.':
                     cur = board[i][j]
                     if (i,cur) in set1 or (cur,j) in set1 or (i//3,j//3,cur) in set1:
