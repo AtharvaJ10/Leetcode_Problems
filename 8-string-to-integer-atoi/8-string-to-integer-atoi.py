7a5b3fa39d11f863a9f1f17e17fe5d486102e015
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-09-03 22:52:01 -0400

    Time: 61 ms (37.21%), Space: 14 MB (28.50%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.py
@@ -2,24 +2,23 @@ class Solution:
     def myAtoi(self, s: str) -> int:
         max1 = 2**31-1
         min1 = -2**31
-        
         s = s.strip()
         sign = 1
-        num = 0
-        index = 0
         if not s:
-            return num
+            return 0
+        
         if s[0]=="-":
             sign = -1
-            index+=1
-        elif s[0]=="+":
-            index+=1
-        
-        while index<len(s) and s[index].isdigit():
-            curr = ord(s[index]) - ord('0')
-            if (num>max1//10) or (num==max1//10 and curr>7):
-                return max1 if sign==1 else min1
-            num = num*10+curr
-            index+=1
+            
+        num = 0
+        i = 0
+        if s[0] in ["+","-"]:
+            i+=1
+        while i<len(s) and s[i].isdigit():
+            new_num = ord(s[i]) - ord("0")
+            if num>max1 or num*10+new_num>max1:
+                return max1 if sign>0 else min1
+            num = num*10+new_num
+            i+=1
         
         return sign*num
