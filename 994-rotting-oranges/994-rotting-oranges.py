0a9e9e2376d6d6cec8171dcbb7921b88d68ba787
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-09-04 14:05:28 -0400

    Time: 68 ms (73.16%), Space: 13.9 MB (91.46%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -1,7 +1,7 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         fresh = 0
-        rows, cols = len(grid), len(grid[0])
+        rows,cols = len(grid), len(grid[0])
         queue = deque([])
         for i in range(rows):
             for j in range(cols):
@@ -13,14 +13,18 @@ class Solution:
         if fresh==0:
             return 0
         
-        
         while queue:
-            i,j, minutes = queue.popleft()
-            for x,y in [i+1,j], [i,j-1], [i-1,j], [i,j+1]:
+            r,c,minutes = queue.popleft()
+            for x,y in [r+1,c], [r,c-1], [r-1,c], [r,c+1]:
                 if 0<=x<rows and 0<=y<cols and grid[x][y]==1:
                     fresh-=1
                     if fresh==0:
                         return minutes+1
-                    grid[x][y] = 2
+                    grid[x][y]=0
                     queue.append([x,y,minutes+1])
-        return -1
+                    
+        if fresh:
+            return -1
+        else:
+            return 0
+                    
