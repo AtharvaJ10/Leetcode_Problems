8db89c8a63c1d3e4eee630bcae6e232f92c0f66b
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-09-05 19:52:19 -0400

    Time: 226 ms (96.02%), Space: 14 MB (74.45%) - LeetHub

--- 688-knight-probability-in-chessboard/688-knight-probability-in-chessboard.py
+++ 688-knight-probability-in-chessboard/688-knight-probability-in-chessboard.py
@@ -1,19 +1,19 @@
 class Solution:
     def knightProbability(self, n: int, k: int, row: int, column: int) -> float:
-        moves = [(1,2),(-1,2),(1,-2),(-1,-2),(2,1),(2,-1),(-2,1),(-2,-1)]
-        memo, out_board_p = {(row, column): 1}, 0
-        # for each step we will create a new dict to record the onboard coordinates and their probabilities.
-        for step in range(k):
-            next_memo = collections.defaultdict(int)
-            for (i, j), prob in memo.items():
-                for d in moves:
-                    di, dj = i+d[0], j+d[1]
-                    # if the next step is on the board, we record it for next step's calculation
-                    if 0<=di<n and 0<=dj<n:
-                        next_memo[(di,dj)] += prob * 0.125
-                    # if the next step is not on the board, we sum it to our accumulate probability of out-board.
+        queue = defaultdict(int)
+        queue[(row,column)] = 1
+        out_prob = 0
+        dirs = [[-2,-1], [-1,-2],[1,-2],[2,-1],[2,1],[1,2],[-1,2],[-2,1]]
+        for _ in range(k):
+            layer = defaultdict(int)
+            for (r,c),prob in queue.items():
+                for i,j in dirs:
+                    x,y = r+i, c+j
+                    if 0<=x<n and 0<=y<n:
+                        layer[(x,y)]+=prob*0.125
                     else:
-                        out_board_p += prob * 0.125
-            memo = next_memo
-        # the on-board prob = 1 - the accumulate out board prob
-        return 1-out_board_p
+                        out_prob+=prob*0.125
+            queue = layer
+        return 1-out_prob
+                
+                
