84bbd7c6816a30ade75d47ede3443539de52009a
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-09-05 21:34:29 -0400

    Time: 1126 ms (26.16%), Space: 22.3 MB (16.24%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -1,20 +1,19 @@
 class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
-        rows,cols = len(grid), len(grid[0])
-        if k >= rows + cols - 2: return rows + cols - 2  # if we can go by manhattan distance -> let's go
         queue = deque([])
-        queue.append([0,0,k,0])
+        queue.append([0,0,0,k])
+        rows, cols = len(grid), len(grid[0])
         visited = set()
         visited.add((0,0,k))
         while queue:
-            i,j,k,step = queue.popleft()
-            if (i,j)==(rows-1,cols-1):
+            r,c,step,k = queue.popleft()
+            if (r,c)==(rows-1,cols-1):
                 return step
             
-            for x,y in [i+1,j], [i,j-1], [i-1,j], [i,j+1]:
+            for x,y in [r+1,c], [r,c-1], [r-1,c], [r,c+1]:
                 if 0<=x<rows and 0<=y<cols:
-                    new_k = k-grid[x][y]
+                    new_k=k-grid[x][y]
                     if new_k>=0 and (x,y,new_k) not in visited:
                         visited.add((x,y,new_k))
-                        queue.append([x,y,new_k, step+1])
+                        queue.append([x,y,step+1,new_k])
         return -1
