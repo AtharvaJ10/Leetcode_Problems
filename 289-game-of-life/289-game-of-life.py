2ffcd88c22618a8eb2b4de838408d8cce018c5ea
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-09-06 12:13:08 -0400

    Time: 60 ms (28.53%), Space: 14 MB (9.92%) - LeetHub

--- 289-game-of-life/289-game-of-life.py
+++ 289-game-of-life/289-game-of-life.py
@@ -1,29 +1,28 @@
 class Solution:
-    def gameOfLife(self, board: List[List[int]]) -> None:
+    def gameOfLife(self, grid: List[List[int]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
-        rows, cols = len(board), len(board[0])
+        rows, cols = len(grid), len(grid[0])
         for i in range(rows):
             for j in range(cols):
                 live = 0
-                for x,y in [i-1,j-1],[i-1,j],[i-1,j+1],[i,j+1],[i+1,j+1],[i+1,j],[i+1,j-1],[i,j-1]:
-                    if 0<=x<rows and 0<=y<cols:
-                        if abs(board[x][y])==1:
-                            live+=1
-                if board[i][j]==1:
+                for x,y in [i+1,j], [i+1,j-1], [i,j-1], [i-1,j-1], [i-1,j], [i-1,j+1], [i,j+1], [i+1,j+1]:
+                    if 0<=x<rows and 0<=y<cols and abs(grid[x][y])==1:
+                        live+=1
+                if grid[i][j]==1:
                     if live<2 or live>3:
-                        board[i][j] = -1
-                else:
+                        grid[i][j] = -1
+                elif grid[i][j]==0:
                     if live==3:
-                        board[i][j]=2
-                #print(board,i,j,live)
+                        grid[i][j] = 2
         
         for i in range(rows):
             for j in range(cols):
-                if board[i][j]==-1:
-                    board[i][j] = 0
-                elif board[i][j]==2:
-                    board[i][j]=1
-        
+                if grid[i][j]==-1:
+                    grid[i][j] = 0
+                elif grid[i][j]==2:
+                    grid[i][j] = 1
+                    
+                    
         
