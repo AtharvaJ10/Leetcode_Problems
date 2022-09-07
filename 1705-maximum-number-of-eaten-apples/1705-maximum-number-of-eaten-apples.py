e207fbbef277d2574eec885a5b8e8939d7f13794
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-09-06 22:28:56 -0400

    Time: 1899 ms (5.21%), Space: 18.7 MB (26.02%) - LeetHub

--- 1705-maximum-number-of-eaten-apples/1705-maximum-number-of-eaten-apples.py
+++ 1705-maximum-number-of-eaten-apples/1705-maximum-number-of-eaten-apples.py
@@ -1,17 +1,17 @@
 class Solution:
-    def eatenApples(self, A: List[int], D: List[int]) -> int:
-        ans, i, N = 0, 0, len(A)
-        h = []
-        while i < N or h:
-            # only push to heap when you have a valid i, and the apple has atleast one day to stay fresh.
-            if i<N and A[i] > 0:
-                heappush(h, [i+D[i], A[i]])
-            # remove the rotten apples batches and the batches with no apples left (which might have got consumed).
-            while h and (h[0][0] <= i or h[0][1] <= 0):
-                heappop(h)
-            # only if there is batch in heap after removing all the rotten ones, you can eat. else wait for the subsequent days for new apple batch by incrementing i.
-            if h:
-                h[0][1]-=1
-                ans+=1
+    def eatenApples(self, apples: List[int], days: List[int]) -> int:
+        heap = []
+        i = 0
+        res = 0
+        while i<len(apples) or heap:
+            if i<len(apples) and apples[i]>0:
+                heapq.heappush(heap, [i+days[i], apples[i]])
+            
+            while heap and (heap[0][0]<=i or heap[0][1]<=0):
+                heapq.heappop(heap)
+            
+            if heap:
+                heap[0][1]-=1
+                res+=1
             i+=1
-        return ans 
+        return res
