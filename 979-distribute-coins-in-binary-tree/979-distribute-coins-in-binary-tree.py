867c8fc45dae9dfc5697d67b30d476a479ac5473
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-09-08 12:36:06 -0400

    Time: 38 ms (93.28%), Space: 13.8 MB (74.93%) - LeetHub

--- 979-distribute-coins-in-binary-tree/979-distribute-coins-in-binary-tree.py
+++ 979-distribute-coins-in-binary-tree/979-distribute-coins-in-binary-tree.py
@@ -12,7 +12,7 @@ class Solution:
                 return 0
             left = dfs(node.left)
             right = dfs(node.right)
-            self.ans += abs(left) + abs(right)
+            self.ans+=abs(left)+abs(right)
             return node.val+left+right-1
         dfs(root)
         return self.ans
