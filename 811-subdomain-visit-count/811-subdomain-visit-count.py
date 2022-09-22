e6e7354e2f6903f1941170d4f13e5ae05b46d46f
AtharvaJ10 <49825649+AtharvaJ10@users.noreply.github.com>
2022-09-21 22:16:06 -0400

    Time: 133 ms (6.16%), Space: 13.9 MB (81.24%) - LeetHub

--- 811-subdomain-visit-count/811-subdomain-visit-count.py
+++ 811-subdomain-visit-count/811-subdomain-visit-count.py
@@ -2,14 +2,12 @@ class Solution:
     def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
         d = defaultdict(int)
         for domain in cpdomains:
-            count, url = domain.split(" ", 1)
-            sub_domain = url.split(".")
-            for i in range(len(sub_domain)):
-                d[tuple(sub_domain[i:])]+=int(count)
+            count, subDomain = domain.split(" ", 1)
+            temp = subDomain.split(".")
+            for i in range(len(temp)):
+                d[tuple(temp[i:])]+=int(count)
         
         res = []
-        for key in d:
-            temp = '.'.join(key)
-            res.append(str(d[key]) + " "+ temp)
+        for i in d:
+            res.append(str(d[i])+" "+ ".".join(i))
         return res
-                       
